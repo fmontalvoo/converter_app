7f4f075385a4a2819832c537f2dd1270a4891380
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-02-26 20:17:59 -0500

    Personalizo tema de la aplicacion.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- app/src/main/java/com/fmontalvoo/converter/MainActivity.java
+++ app/src/main/java/com/fmontalvoo/converter/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         converterController = new ConverterController();
 
